e78f491686de5e2c2a8ab5745f42da0d6d69ea08
milanchahar <milanchahar13@gmail.com>
2025-11-30 19:00:11 +0530

    updates

--- frontend/src/components/SQLEditor.jsx
+++ frontend/src/components/SQLEditor.jsx
@@ -1,7 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import { EditorState } from "@codemirror/state";
 import { EditorView, keymap } from "@codemirror/view";
-
+import { defaultKeymap } from "@codemirror/commands";
+f
 export default function SQLEditor({ value, onChange, onExecute, onFocus }) {
   const editorRef = useRef(null);
   const [tables, setTables] = useState({});
