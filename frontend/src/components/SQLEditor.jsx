dbc388b3cb98b604a04d0ddfcddb02b49043a7b4
milanchahar <milanchahar13@gmail.com>
2025-11-30 19:02:14 +0530

    updates

--- frontend/src/components/SQLEditor.jsx
+++ frontend/src/components/SQLEditor.jsx
@@ -4,6 +4,7 @@ import { EditorView, keymap } from "@codemirror/view";
 import { defaultKeymap } from "@codemirror/commands";
 import { sql } from "@codemirror/lang-sql";
 import { autocompletion } from "@codemirror/autocomplete";
+import { defaultKeymap } from "@codemirror/commands";
 
 export default function SQLEditor({ value, onChange, onExecute, onFocus }) {
   const editorRef = useRef(null);
